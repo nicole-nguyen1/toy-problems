dfd81e0067a82fe54cf82c7ebf01c5d6ba10ea1a
Nicole Nguyen <ninguyen00@gmail.com>
2018-10-01 21:26:14 -0500

    Complete three sum with two pointers

--- threeSumTwoPointer.js
+++ threeSumTwoPointer.js
@@ -1,44 +1,40 @@
 const threeSumTwoPointer = (nums) => {
   let solutions = [];
 
-  if (!nums || nums.length < 3) {
+  if (nums.length < 3) {
     return solutions;
   }
-
   nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length - 2; i++) {
-    if (i === 0 || nums[i] > nums[i-1]) {
-      let j = i + 1;
-      let k = nums.length - 1;
+    if (nums[i] > 0) {
+      return solutions;
+    }
+    if (i > 0 && nums[i] == nums[i - 1]) {
+      continue;
+    }
 
-      while (j < k) {
-        let sum = nums[i] + nums[j] + nums[k];
-        let triplet = [nums[i], nums[j], nums[k]];
+    let j = i + 1;
+    let k = nums.length - 1;
 
-        if (sum === 0) {
-          solutions.push(triplet);
+    while (j < k) {
+      if (nums[i] + nums[j] + nums[k] === 0) {
+        solutions.push([nums[i], nums[j], nums[k]]);
+        j++;
+        k--;
+        while (nums[j] === nums[j - 1]) {
           j++;
-          k--;
-
-          //skip duplicates from k iterator
-          while (nums[j + 1] === nums[j]) {
-            j++;
-          }
-
-          // skip duplicates from j iterator
-          while (nums[k - 1] === nums[k]) {
-            k--;
-          }
-        } else if (sum < 0) { //increase lower number
-          j++;
-        } else { //decrease upper number
+        }
+        while (nums[k] === nums[k + 1]) {
           k--;
         }
-      } 
+      } else if (nums[i] + nums[j] + nums[k] > 0) {
+        k--;
+      } else {
+        j++;
+      }
     }
   }
-
   return solutions;
 }
 
